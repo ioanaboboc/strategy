448d5f5e6ae6acaac519336bad5c3e36c28f0d43
Ioana Boboc <ioanaboboc1@gmail.com>
2013-02-14 17:53:27 +0200

    Commit 6

--- strategy/src/EMA.java
+++ strategy/src/EMA.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 public class EMA {
 	private double k;
 	private Double previous = null;
-	private ArrayList<Double> history;
 	private int days;
+	private int in_days;
 	public EMA(int days){
 		k = 2.0/(days+1);
-		history = new ArrayList<Double>();
+		in_days = 0;
 		this.days = days;
 	}
 	
 	//Calculate current value of EMA: k*Price(t)+(1-k)*EMA(t-1)
 	public Double iterate(Double val){
-		if(history.size()<days){
-			history.add(val);
+		if(in_days<days){
+			++in_days;
 			return null;
 		}
-		else if(history.size()==days && previous==null){
-			previous=MathOp.avg(history);
+		else if(in_days==days){
+			previous=(previous*in_days+val)/(++in_days);
 			return previous;
 		}
 		previous = new Double (k*val+(1-k)*previous);
